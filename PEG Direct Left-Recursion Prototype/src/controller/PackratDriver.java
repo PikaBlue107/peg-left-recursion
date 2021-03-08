097ba20f8ced43ddf1fee61c640519db767f9386
jvgriese <jvgriese@ncsu.edu>
2021-03-08 12:29:25 -0500

    Restore PackratDriver to original state

--- PEG Direct Left-Recursion Prototype/src/controller/PackratDriver.java
+++ PEG Direct Left-Recursion Prototype/src/controller/PackratDriver.java
@@ -1,6 +1,9 @@
 package controller;
 
-import java.io.File;
+import patterns.definition.DefinitionExpression;
+import patterns.general.Pattern;
+import structure.InputContext;
+import structure.Result;
 
 public class PackratDriver {
 
@@ -14,28 +17,25 @@ public class PackratDriver {
 	 */
 	public static void main(final String[] args) {
 
-//		// Create the Input context
-//		final InputContext input = new InputContext(TEST_STRING);
-//
-//		// Create the pattern
-//		final Pattern matcher = new DefinitionExpression();
-//
-//		// Attempt to match
-//		final Result result = matcher.lazyMatch(input);
-//
-//		// Print out the result
-//		System.out.println(result.toString());
-//
-//		System.out.println("Hidden tree:");
-//		System.out.println(result.printResultTree());
-//		System.out.println("\n\n\n\n\n");
-//
-//		System.out.println("Full tree:");
-//		System.out.println(result.printResultTree(true));
-//		System.out.println("\n\n\n\n\n");
+		// Create the Input context
+		final InputContext input = new InputContext(TEST_STRING);
 
-		final File test = new File("path/to/");
-		test.mkdirs();
+		// Create the pattern
+		final Pattern matcher = new DefinitionExpression();
+
+		// Attempt to match
+		final Result result = matcher.lazyMatch(input);
+
+		// Print out the result
+		System.out.println(result.toString());
+
+		System.out.println("Hidden tree:");
+		System.out.println(result.printResultTree());
+		System.out.println("\n\n\n\n\n");
+
+		System.out.println("Full tree:");
+		System.out.println(result.printResultTree(true));
+		System.out.println("\n\n\n\n\n");
 
 	}
 }
