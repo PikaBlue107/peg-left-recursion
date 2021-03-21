0f0e847ee5cb346ffc41d6dc4f271445b2c2de7a
jvgriese <jvgriese@ncsu.edu>
2021-03-21 00:54:09 -0400

    Refactor main driver for easier testing

--- PEG Direct Left-Recursion Prototype/src/controller/PackratDriver.java
+++ PEG Direct Left-Recursion Prototype/src/controller/PackratDriver.java
@@ -5,10 +5,20 @@ import patterns.general.Pattern;
 import structure.InputContext;
 import structure.Result;
 
+@SuppressWarnings("unused")
+/**
+ * Main launch point for the program
+ * 
+ * @author Melody Griesen
+ *
+ */
 public class PackratDriver {
 
 	/** Test string to use for each run. */
 	private static final String TEST_STRING = "1+25+7";
+
+	/** Pattern used to match the test string. */
+	private static final Pattern MATCH_PATTERN = new DefinitionExpression();
 
 	/**
 	 * Main launch point of the program. Sandbox. Playground. Laboratory.
@@ -16,12 +26,20 @@ public class PackratDriver {
 	 * @param args command-line arguments, unused.
 	 */
 	public static void main(final String[] args) {
+//		doMatch(TEST_STRING, MATCH_PATTERN);
+		System.out.println(InputContext.CHAR_EPSILON);
+	}
+
+	/**
+	 * Do the specified match
+	 *
+	 * @param inputString the string to match
+	 * @param matcher     the pattern to match with
+	 */
+	public static void doMatch(final String inputString, final Pattern matcher) {
 
 		// Create the Input context
-		final InputContext input = new InputContext(TEST_STRING);
-
-		// Create the pattern
-		final Pattern matcher = new DefinitionExpression();
+		final InputContext input = new InputContext(inputString);
 
 		// Attempt to match
 		final Result result = matcher.lazyMatch(input);
@@ -36,6 +54,5 @@ public class PackratDriver {
 		System.out.println("Full tree:");
 		System.out.println(result.printResultTree(true));
 		System.out.println("\n\n\n\n\n");
-
 	}
 }
