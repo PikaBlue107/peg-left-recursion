f46c386adf0b982e36fa6751d3f88b7c8bee9115
jvgriese <jvgriese@ncsu.edu>
2021-03-21 00:51:57 -0400

    Add MemoryEvent to track interactions with saved data

---
+++ PEG Direct Left-Recursion Prototype/src/event/control/MemoryEvent.java
@@ -0,0 +1,80 @@
+/**
+ * 
+ */
+package event.control;
+
+import patterns.general.Pattern;
+import structure.InputContext;
+
+/**
+ * @author Melody Griesen
+ *
+ */
+public class MemoryEvent extends ControlEvent {
+
+	/**
+	 * Possible types for a PositionEvent.
+	 * 
+	 * @author Melody Griesen
+	 *
+	 */
+	public enum MemoryEventTYpe {
+		/** Memory was checked. */
+		CHECK("Check"),
+		/** Memory was saved. */
+		SAVE("Save"),
+		/** Memory was assumed as the result of a match. */
+		ASSUME("Assume");
+
+		/** Stores a display-friendly name for this enum. */
+		private String displayName;
+
+		/**
+		 * Constructs each Enum value with a display-friendly name
+		 *
+		 * @param name the name to display for this Enum value
+		 */
+		MemoryEventTYpe(final String name) {
+			displayName = name;
+		}
+
+		/**
+		 * Retrieves the display name for this enum
+		 *
+		 * @return this enum's display name
+		 */
+		public String getDisplayName() {
+			return displayName;
+		}
+	}
+
+	/** Which sub-type this event falls under. */
+	private final MemoryEventTYpe type;
+
+	/**
+	 * Creates a MemoryEvent to be logged for checking, saving, or assuming a match
+	 * in memory. Should be called *before* making any position changes as part of
+	 * an ASSUME operation.
+	 * 
+	 * @param context the InputContext storing the saved results
+	 * @param type    whether this memory event is a check, a save, or an assume
+	 * @param pattern the pattern that this memory event is using
+	 * @param result  the string saved in memory (may be null)
+	 */
+	public MemoryEvent(final InputContext context, final MemoryEventTYpe type, final Pattern pattern,
+			final String result) {
+		super(context, context.getPosition(), result.length(), pattern.getType() + " = " + result);
+		this.type = type;
+	}
+
+	/**
+	 * Returns this position event's PositionEventType.
+	 * 
+	 * @return the display name for this event type
+	 */
+	@Override
+	public String getType() {
+		return type.getDisplayName();
+	}
+
+}
